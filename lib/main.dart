0e052324b60e53796a2fe5959a88414892e5e101
yaraFarouk <yaraFarouk960@gmail.com>
2024-10-26 13:32:06 +0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,22 @@
+import 'package:fashion_assistant/screens/home_screen.dart';
+import 'package:fashion_assistant/screens/total_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(const FashionAssistant());
 }
+
 class FashionAssistant extends StatelessWidget {
   const FashionAssistant({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ScreenUtilInit(
+        designSize: const Size(375, 812),
+        minTextAdapt: true,
+        builder: (context, child) {
+          return const MaterialApp(home: TotalScreens());
+        });
   }
 }
