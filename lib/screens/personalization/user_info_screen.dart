520596109ab98a02b6b3e660956f2c23876486dd
Karim Yasser <karimmyasserr@gmail.com>
2024-12-01 05:16:14 +0200

    🔨some edits in code format of profile

--- lib/screens/personalization/user_info_screen.dart
+++ lib/screens/personalization/user_info_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
 
 import '../../constants.dart';
+import '../../widgets/profile_page/profile_menu.dart';
 
 class UserInfoScreen extends StatelessWidget {
   const UserInfoScreen({super.key});
@@ -47,11 +48,23 @@ class UserInfoScreen extends StatelessWidget {
               const SizedBox(height: Sizes.spaceBtwItems),
 
               ProfileMenu(
-                  title: 'First Name', value: 'Karim', onPressed: () {}),
+                title: 'First Name',
+                value: 'Karim',
+                onPressed: () {},
+                icon: Iconsax.arrow_right_34,
+              ),
               ProfileMenu(
-                  title: 'Last Name', value: 'Yasser', onPressed: () {}),
+                title: 'Last Name',
+                value: 'Yasser',
+                onPressed: () {},
+                icon: Iconsax.arrow_right_34,
+              ),
               ProfileMenu(
-                  title: 'UserName', value: 'Karimmyasser', onPressed: () {}),
+                title: 'UserName',
+                value: 'Karimmyasser',
+                onPressed: () {},
+                icon: Iconsax.copy,
+              ),
 
               const SizedBox(height: Sizes.spaceBtwItems),
               const Divider(),
@@ -62,65 +75,49 @@ class UserInfoScreen extends StatelessWidget {
               const SizedBox(height: Sizes.spaceBtwItems),
 
               ProfileMenu(
-                  title: 'User ID', value: '123e4567-e89b-12d3-a456-426614174000', onPressed: () {},icon: Iconsax.copy),
+                title: 'User ID',
+                value: '123e4567-e89b-12d3-a456-426614174000',
+                onPressed: () {},
+                icon: Iconsax.copy,
+              ),
               ProfileMenu(
-                  title: 'E-mail', value: 'kemoyasso66@gmail.com', onPressed: () {}),
+                title: 'E-mail',
+                value: 'kemoyasso66@gmail.com',
+                onPressed: () {},
+                icon: Iconsax.arrow_right_34,
+              ),
               ProfileMenu(
-                  title: 'Phone Number', value: '+201144432284', onPressed: () {}),
+                title: 'Phone Number',
+                value: '+201144432284',
+                onPressed: () {},
+                icon: Iconsax.arrow_right_34,
+              ),
               ProfileMenu(
-                  title: 'Gender', value: 'Male', onPressed: () {}),
+                title: 'Gender',
+                value: 'Male',
+                onPressed: () {},
+                icon: Iconsax.arrow_right_34,
+              ),
               ProfileMenu(
-                  title: 'Birth Date', value: '07/05/2003', onPressed: () {}),
+                title: 'Birth Date',
+                value: '07/05/2003',
+                onPressed: () {},
+                icon: Iconsax.arrow_right_34,
+              ),
               Center(
-                child: TextButton(
-                    onPressed: () {},
-                    child: const Text('Close Account',
-                        style: TextStyle(color: OurColors.errorTextColor))),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextButton(
+                      onPressed: () {},
+                      child: Text('Close Account',
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleMedium!
+                              .apply(color: OurColors.errorTextColor))),
+                ),
               )
             ],
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class ProfileMenu extends StatelessWidget {
-  const ProfileMenu({
-    super.key,
-    this.icon = Iconsax.arrow_right_34,
-    required this.onPressed,
-    required this.title,
-    required this.value,
-  });
-
-  final IconData icon;
-  final VoidCallback onPressed;
-  final String title, value;
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onPressed,
-      child: Padding(
-        padding:
-            const EdgeInsets.symmetric(vertical: Sizes.spaceBtwItems / 1.5),
-        child: Row(
-          children: [
-            Expanded(
-              flex: 3,
-              child: Text(title,
-                  style: Theme.of(context).textTheme.bodySmall,
-                  overflow: TextOverflow.ellipsis),
-            ),
-            Expanded(
-              flex: 5,
-              child: Text(value,
-                  style: Theme.of(context).textTheme.bodyMedium,
-                  overflow: TextOverflow.ellipsis),
-            ),
-            Expanded(child: Icon(icon, size: 18))
-          ],
         ),
       ),
     );
